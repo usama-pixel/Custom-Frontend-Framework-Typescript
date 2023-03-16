7631341268d1c24df3e30ba0936fff770ddb107f
usama <usamaali052@gmail.com>
2023-03-17 02:08:45 +0500

    framework so far

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there')
+import { User } from './models/User';
+
+const user = new User({id: 1});
+
+// console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log(user);
+});
+
+user.fetch()
+
+// user.set({name: 'new Naam'})
+
+// user.trigger('change');
